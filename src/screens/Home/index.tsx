dd17149794360441cd37d8dc11c6a9e03b57c589
Lubni Morais <lubni.morais@gmail.com>
2024-03-06 22:09:57 -0300

    First commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+import { styles } from "./styles"
+
+export const HomeScreen = () => {
+  return (
+    <View style={styles.homeContainer}>
+      <Text>Home</Text>
+    </View>
+  )
+}
